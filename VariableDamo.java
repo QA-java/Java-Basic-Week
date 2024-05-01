6c078be0bda97f71f5919db743f3136dcc6d606a
NadeemAhamad-QA <nadeemahamadqa@gmail.com>
2024-05-01 12:00:14 +0530

    Variable First Damo

---
+++ VariableDamo.java
@@ -0,0 +1,34 @@
+public class VariableDamo {
+
+    public static void main(String... arg) {
+
+        System.out.println("Second Program");
+
+        /* Approch 1
+         * int a = 100;
+         * int b = 200;
+         * int c = 300;
+         * 
+         * or
+         * Approch 2
+         * int a, b, c;
+         * a = 1001;
+         * b = 200;
+         * c= 300;
+         * 
+         * or
+         * Approch 3
+         * 
+         * int a = 100, b = 200, c= 300;
+         * 
+         * ALL ARE SAME
+         */
+
+        int a = 100, b = 200, c = 300;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(a + b - c);
+    }
+}
